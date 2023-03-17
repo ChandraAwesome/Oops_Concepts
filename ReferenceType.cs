58e98f4c33891a6a03b349c5eac1084f01006fd2
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-17 18:59:16 +0530

    [Chandra]

---
+++ ReferenceType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_with_oops
+{
+    class ReferenceType
+    {
+        public string value
+        {
+            get;
+            set;
+        }
+        public ReferenceType(string passbyref)
+        {
+            this.value = passbyref;
+        }
+    }
+    
+}
