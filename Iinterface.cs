cb2ee4ed786c6c517b7c1b712b850077c4dd7f76
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-17 19:03:27 +0530

    [Chandra]

---
+++ Iinterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_with_oops
+{
+     interface Iinterface
+    {
+        void method1();
+        //void method2();
+    }
+
+    interface Inone
+    {
+        void method2();
+    }
+    class onee : Iinterface, Inone
+    {
+        public void method1()
+        {
+            Console.WriteLine("Interface");
+        }
+
+        public void method2() { Console.WriteLine("Interface2"); }
+
+    }
+}
