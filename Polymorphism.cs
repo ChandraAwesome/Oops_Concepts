d9eb02d78e5120d3aa1cc7c5524a9a07bdbf65d2
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-17 18:58:49 +0530

    [Chandra]

---
+++ Polymorphism.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_with_oops
+{
+    public class Polymorphism
+    {
+        //public string Concept = "Oops";
+
+        public virtual void Oops() 
+        {
+            Console.WriteLine("The concepts of opps are :");
+        }
+    }
+    class innherit
+    {
+        public  void Oops()
+        {
+            Console.WriteLine("Inheritance");
+        }
+    }
+    class Poly
+    {
+        public void Oops()
+        {
+            Console.WriteLine("Polymorphism");
+        }
+    }
+}
