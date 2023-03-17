ce58cdd8e13d5ce86abba60ef39997c891a8a633
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-17 19:00:14 +0530

    [Chandra]

---
+++ ValueType_ReferenceType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Learning_with_oops
+{
+    public class ValueType_ReferenceType
+    {
+        public static void Value()
+        {
+            string Name = "Ajay";
+            Console.WriteLine(Name);
+            ChangeValue(Name);
+
+        }
+
+        public static void ChangeValue(string Names)
+        {
+            Names = "Chandra";
+            Console.WriteLine(Names);
+        }
+    }
+}
