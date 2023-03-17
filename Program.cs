e0402641b05a14be984b520b7e802d68fa5ce487
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-17 19:04:43 +0530

    [Chandra]

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Learning_with_oops;
+
+using static Learning_with_oops.Polymorphism;
+namespace Learning_with_oops
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           // Constructer obj = new Constructer("tata");
+           // Console.WriteLine(obj.Car);
+
+           // Constructer.Animals animal = new Constructer.Animals();
+           // Inheritance_Animals.Sounds();
+           // Console.WriteLine(animal.Species + "\n" + animal.Breed);
+
+           // Polymorphism one = new Polymorphism();
+           // innherit two = new innherit();
+           // Poly three= new Poly();
+
+           // one.Oops();
+           // two.Oops();
+           // three.Oops();
+
+           // abc a = new abc();
+           // a.method();
+           // a.method2();
+
+           // onee d = new onee();
+           // d.method1();
+           // d.method2();
+
+           //TypesofVariables result= new TypesofVariables();
+           // result.TypesofVariabless();
+
+          //  ValueType_ReferenceType.Value();
+           ReferenceType change= new ReferenceType("Ajay");
+            
+            Console.WriteLine(change.value);
+        }
+    }
+
+}
