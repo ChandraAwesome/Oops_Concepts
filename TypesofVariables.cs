165b88129278f1941c324c3909f9320b9a344677
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-17 18:59:46 +0530

    [Chandra]

---
+++ TypesofVariables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_with_oops
+{
+    internal class TypesofVariables
+    {
+        public void TypesofVariabless()
+        {
+            //public string Types;
+        
+            string Name = "Ajay";
+        int Numbwe = 45;
+        double decimals = 45.01323;
+        float pointss = 45.00f;
+        bool Yes = true;
+        char letter = 'A';
+
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}",Name,Numbwe,decimals,pointss,Yes,letter);
+        }  
+    }
+}
